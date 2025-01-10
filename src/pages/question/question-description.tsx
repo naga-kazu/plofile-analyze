f693d572d0177067cefe779ea497c45d2f93526c
nagasaka-kazuki <nagasaka.kazuki@d-publlishing.jp>
2025-01-10 22:05:31 +0900

    修正

--- src/pages/question/question-description.tsx
+++ src/pages/question/question-description.tsx
@@ -8,10 +8,10 @@ type Props = {
 export function QuestionDescription({ title, children }: Props) {
   return (
     <div className="relative">
-      <p className="absolute -top-8 -left-6 px-3 py-1 text-sm font-bold bg-gray-700 text-white inline-block rounded-sm">
+      <p className="absolute -top-7 -left-6 px-2 py-1 text-sm font-bold bg-gray-700 text-white inline-block rounded-sm">
         {title}
       </p>
-      <div className="space-y-1">{children}</div>
+      <div className="pt-2">{children}</div>
     </div>
   );
 }
