a48adf4204c9610d0fbe3032998d6f3f9a023bae
nagasaka-kazuki <nagasaka.kazuki@d-publlishing.jp>
2025-01-10 13:34:44 +0900

    ホスティング用にプッシュ

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "docs", // ビルド結果を格納するディレクトリ
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
