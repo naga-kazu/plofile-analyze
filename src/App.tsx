565149bc1e5f42e0726dc589d72d6ef56240f8bf
nagasaka-kazuki <nagasaka.kazuki@d-publlishing.jp>
2025-01-10 13:49:02 +0900

    一旦プッシュ

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import FixedHeader from "./components/header";
 import Question from "./pages/question/question";
 import { Empty } from "./pages/empty";
 
-const BASE_PATH = "/plofile-analyze";
+const BASE_PATH = "https://naga-kazu.github.io/plofile-analyze";
 
 export default function QuizPage() {
   return (
     <BrowserRouter>
       <FixedHeader />
       <div className="min-h-screen bg-background p-8 flex items-center justify-center">
-        <Routes>
+        <Question />
+        {/* <Routes>
           <Route path={BASE_PATH} element={<Question />} />
           <Route path={`${BASE_PATH}/empty-page`} element={<Empty />} />
-        </Routes>
+        </Routes> */}
       </div>
     </BrowserRouter>
   );
