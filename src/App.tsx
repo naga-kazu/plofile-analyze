d587631a94bf93f84873c4b4d5ce99771b2e02bd
nagasaka-kazuki <nagasaka.kazuki@d-publlishing.jp>
2025-01-10 19:15:39 +0900

    修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router";
 import FixedHeader from "./components/header";
 import Question from "./pages/question/question";
+import { Empty } from "./pages/empty";
 
 export default function QuizPage() {
   return (
-    // <BrowserRouter>
-    <div>
+    <BrowserRouter>
       <FixedHeader />
       <div className="min-h-screen bg-background p-8 flex items-center justify-center">
-        <Question />
-        {/* <Routes>
-          <Route path={BASE_PATH} element={<Question />} />
-          <Route path={`${BASE_PATH}/empty-page`} element={<Empty />} />
-        </Routes> */}
+        <Routes>
+          <Route path="/" element={<Question />} />
+          <Route path="/empty" element={<Empty />} />
+        </Routes>
       </div>
-    </div>
-    // </BrowserRouter>
+    </BrowserRouter>
   );
 }
