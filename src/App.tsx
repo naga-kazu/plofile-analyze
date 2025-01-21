b60a70a75aa7b27b67c0e9a88d6d7cf5d771715d
naga-kazu <nagasaka317@gmail.com>
2025-01-22 08:16:23 +0900

    修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import {ScrollRestoration, createBrowserRouter, Outlet, RouterProvider } from "react-router";
 import FixedHeader from "./components/header";
 import { Question } from "./pages/question/question";
 import { questions } from "./pages/question/const";
@@ -7,26 +7,49 @@ import Result from "./pages/result/result";
 
 const BASE_URL = "/plofile-analyze";
 
-export default function QuizPage() {
-  return (
-    <BrowserRouter>
-      <FixedHeader />
-      <div className="w-full max-w-md min-w-80 mx-auto mt-3 px-4 pt-20 pb-10 bg-background">
-        <Routes>
-          <Route path={BASE_URL} element={<Top nextPath={`${BASE_URL}/question/1`}/>} />
-        {questions.map((question, index) => {
-          const nextPath = index === questions.length - 1 ? `${BASE_URL}/result` : `${BASE_URL}/question/${index + 2}`;
-          return (
-            <Route
-              key={index}
-              path={`${BASE_URL}/question/${index+1}`}
-              element={<Question {...question} step={index + 1} totalSteps={questions.length} nextPath={nextPath} />}
+const router = createBrowserRouter([
+  {
+    path: BASE_URL,
+    element: (
+      <>
+        <ScrollRestoration />
+        <FixedHeader />
+        <div className="w-full max-w-md min-w-80 mx-auto mt-3 px-4 pt-20 pb-10 bg-background">
+          <Outlet /> {/* 子ルートがここに表示される */}
+        </div>
+      </>
+    ),
+    children: [
+      {
+        path: BASE_URL,
+        element: <Top nextPath={`${BASE_URL}/question/1`} />,
+      },
+      ...questions.map((question, index) => {
+        const nextPath =
+          index === questions.length - 1
+            ? `${BASE_URL}/result`
+            : `${BASE_URL}/question/${index + 2}`;
+        return {
+          path: `${BASE_URL}/question/${index + 1}`,
+          element: (
+            <Question
+              {...question}
+              step={index + 1}
+              totalSteps={questions.length}
+              nextPath={nextPath}
             />
-          );
-        })}
-        <Route path={`${BASE_URL}/result`} element={<Result/>} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+          ),
+        };
+      }),
+      {
+        path: `${BASE_URL}/result`,
+        element: <Result />,
+      },
+    ],
+  },
+]);
+
+// QuizPage コンポーネント
+export default function QuizPage() {
+  return <RouterProvider router={router} />;
 }
